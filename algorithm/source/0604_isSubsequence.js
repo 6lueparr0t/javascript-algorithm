cb488b6a5e464984ff6fa6249fa457e9058847e7
6lueparr0t <6lueparr0t@gmail.com>
2024-04-09 08:10:03 +0900

    feat: 섹션 6-2

---
+++ algorithm/source/0604_isSubsequence.js
@@ -0,0 +1,24 @@
+function isSubsequence(str1, str2) {
+    // good luck. Add any arguments you deem necessary.
+    let holder = 0;
+    let checker = 0;
+
+    while (checker < str2.length) {
+        if(holder > checker) break;
+        if (str1[holder] === str2[checker]) {
+            holder++;
+        }
+        checker++;
+    }
+
+    return str1.length === holder;
+}
+
+console.log(isSubsequence('hello', 'hello world'));
+// true
+console.log(isSubsequence('sing', 'sting'));
+// true
+console.log(isSubsequence('abc', 'abraadabrac'));
+// true
+console.log(isSubsequence('abc', 'acb'));
+// false (order matters)
