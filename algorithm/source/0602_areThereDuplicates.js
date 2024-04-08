7be4484f1d2295204b605e22366ef379aab54aeb
6lueparr0t <6lueparr0t@gmail.com>
2024-04-09 06:41:09 +0900

    feat: 섹션 6-1 수정

--- algorithm/source/0602_areThereDuplicates.js
+++ algorithm/source/0602_areThereDuplicates.js
@@ -1,18 +1,22 @@
 function areThereDuplicates(...arr) {
   // good luck. (supply any arguments you deem necessary.)
   const lookup = {};
-  for (let i = 0; i< arr.length; i++) {
-      let letter = String(arr[i]);
-      if(lookup[letter]) {
-         return true; 
-      } else {
-         lookup[letter] = 1;
-      }
+
+  for (let i = 0; i < arr.length; i++) {
+    let c = String(arr[i]);
+    lookup[c] = ++lookup[c] || 1;
   }
-  
+
+  for (let i = 0; i < arr.length; i++) {
+    let c = String(arr[i]);
+    if (lookup[c] > 1) {
+      return true;
+    }
+  }
+
   return false;
 }
 
 console.log(areThereDuplicates(1, 2, 3)); // false
-console.log(areThereDuplicates(1, 2, 2)); // true 
-console.log(areThereDuplicates('a', 'b', 'c', 'a')); // true 
+console.log(areThereDuplicates(1, 2, 2)); // true
+console.log(areThereDuplicates("a", "b", "c", "a")); // true
