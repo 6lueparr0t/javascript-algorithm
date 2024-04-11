1ab6d70b7033dd76ee12167f67d0f24cd69cf38b
6lueparr0t <6lueparr0t@gmail.com>
2024-04-12 07:57:52 +0900

    feat: 섹션 6 완료

--- algorithm/source/0606_minSubarrayLen.js
+++ algorithm/source/0606_minSubarrayLen.js
@@ -1,45 +1,29 @@
 function minSubArrayLen(arr, num) {
-     let sum = 0;
-     let start = 0;
-     let counter = 0;
-     let i = 0;
-    let test = [];
+  let sum = 0;
+  let start = 0;
+  let end = 0;
+  let counter = Infinity;
 
-     while (start < arr.length) {
-         if (arr[i] > num) return 1;
+  while (start < arr.length) {
+    if (sum < num && end < arr.length) {
+      sum += arr[end];
+      end++;
+    } else if (sum >= num) {
+      counter = Math.min(counter, end - start);
+      sum -= arr[start];
+      start++;
+    } else {
+      break;
+    }
+  }
 
-         if (sum < num) {
-             sum += arr[i];
-             test.push(arr[i]);
-         } else {
-             if(i < arr.length) {
-                 sum = sum + arr[i] - arr[start];
-                 test.push(arr[i]);
-                 test=test.slice(1);
-             } else {
-                 sum = sum - arr[start];
-                 test=test.slice(1);
-             }
-             start++;
-         }
+  return counter === Infinity ? 0 : counter;
+}
 
-         console.log(test);
-         if(i < arr.length) {
-             i++;
-         }
-
-         if (sum === num && i === arr.length) {
-             return counter;
-         }
-     }
- 
-     return counter;
- }
-
-console.log(minSubArrayLen([2,3,1,2,4,3], 7)) // 2 -> because [4,3] is the smallest subarray
-// console.log(minSubArrayLen([2,1,6,5,4], 9)) // 2 -> because [5,4] is the smallest subarray
-// console.log(minSubArrayLen([3,1,7,11,2,9,8,21,62,33,19], 52)) // 1 -> because [62] is greater than 52
-// console.log(minSubArrayLen([1,4,16,22,5,7,8,9,10],39)) // 3
-// console.log(minSubArrayLen([1,4,16,22,5,7,8,9,10],55)) // 5
-// console.log(minSubArrayLen([4, 3, 3, 8, 1, 2, 3], 11)) // 2
-// console.log(minSubArrayLen([1,4,16,22,5,7,8,9,10],95)) // 0
+console.log(minSubArrayLen([2, 3, 1, 2, 4, 3], 7)); // 2 -> because [4,3] is the smallest subarray
+console.log(minSubArrayLen([2, 1, 6, 5, 4], 9)); // 2 -> because [5,4] is the smallest subarray
+console.log(minSubArrayLen([3, 1, 7, 11, 2, 9, 8, 21, 62, 33, 19], 52)); // 1 -> because [62] is greater than 52
+console.log(minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 39)); // 3
+console.log(minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 55)); // 5
+console.log(minSubArrayLen([4, 3, 3, 8, 1, 2, 3], 11)); // 2
+console.log(minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 95)); // 0
---
+++ algorithm/source/0607_findLongestSubstring.js
@@ -0,0 +1,33 @@
+function findLongestSubstring(str) {
+  // add whatever parameters you deem necessary - good luck!
+  let word = 0;
+  let start = 0;
+  let end = 0;
+  let counter = 0;
+
+  while (start < str.length) {
+    if (end < str.length) {
+      end++;
+    } else {
+        start++;
+    }
+
+    let sliceStr = str.slice(start, end);
+    let setStr = Array.from(new Set(sliceStr)).join("");
+    counter = Math.max(counter, setStr.length);
+
+    if (sliceStr.length > setStr.length) {
+        start++;
+    }
+  }
+
+  return counter;
+}
+
+console.log(findLongestSubstring("")); // 0
+console.log(findLongestSubstring("rithmschool")); // 7
+console.log(findLongestSubstring("thisisawesome")); // 6
+console.log(findLongestSubstring("thecatinthehat")); // 7
+console.log(findLongestSubstring("bbbbbb")); // 1
+console.log(findLongestSubstring("longestsubstring")); // 8
+console.log(findLongestSubstring("thisishowwedoit")); // 6
