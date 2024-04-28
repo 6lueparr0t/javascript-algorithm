f2a329137306f127bb1d87db0afd3d9364eb8bb0
6lueparr0t <6lueparr0t@gmail.com>
2024-04-28 23:25:07 +0900

    feat: add hackerrank exam

---
+++ hackerrank/etc_bracketChecker.js
@@ -0,0 +1,18 @@
+function bracketChecker(str) {
+    const open = ['{', '[', '('];
+    const close = ['}', ']', ')'];
+
+    const result = [0,0];
+    for(let c of str) {
+        if (open.includes(c)) {
+            result[0]++;
+        } else if (close.includes(c)) {
+            result[1]++;
+        }
+    }
+
+    return result[0] - result[1] === 0 ? true : false;
+}
+
+console.log(bracketChecker('[][]{{()}}')) // true
+console.log(bracketChecker('[][]{{(}}')) // false
---
+++ hackerrank/hkrk_sockMerchant.js
@@ -0,0 +1,19 @@
+// 양말 짝 찾기 문제
+function sockMerchant(n, ar) {
+    // Write your code here
+    const socks = {};
+    for(let i=0; i<n; i++) {
+        socks[ar[i]] = (socks[ar[i]] || 0) + 1; 
+    }
+    
+    let result = 0;
+    for(let key in socks) {
+        if(socks[key] >= 2) {
+            result += Math.floor(socks[key] / 2);
+        }
+    }
+    
+    return result;
+}
+
+console.log(sockMerchant(9, [1,2,2,1,1,3,5,1,2]));
