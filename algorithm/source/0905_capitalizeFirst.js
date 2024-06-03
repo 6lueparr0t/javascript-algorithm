908033e2b37b8fce7e98499f00461b00d91a2a24
6lueparr0t <6lueparr0t@gmail.com>
2024-06-03 18:18:14 +0900

    feat: add 0905_capitalizeFirst2

--- algorithm/source/0905_capitalizeFirst.js
+++ algorithm/source/0905_capitalizeFirst.js
@@ -9,4 +9,16 @@ function capitalizeFirst(arr) {
   return res;
 }
 
+function capitalizeFirst2(arr) {
+  if (arr.length === 1) {
+    return [arr[0][0].toUpperCase() + arr[0].substr(1)];
+  }
+
+  const res = capitalizeFirst(arr.slice(0, -1));
+  res.push(arr.slice(arr.length - 1)[0][0].toUpperCase() + arr.slice(arr.length - 1)[0].substr(1));
+
+  return res;
+}
+
 console.log(capitalizeFirst(["car", "taco", "banana"])); // ['Car','Taco','Banana']
+console.log(capitalizeFirst2(["car", "taco", "banana"])); // ['Car','Taco','Banana']
