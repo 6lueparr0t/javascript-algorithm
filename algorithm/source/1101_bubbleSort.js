fedb58bcb7d1aad9ef864ac957661b8d0cdc204b
Daehyun Lim (임대현) <6lueparr0t@gmail.com>
2024-08-06 08:54:17 +0900

    1101_bubbleSort.js 업데이트

--- algorithm/source/1101_bubbleSort.js
+++ algorithm/source/1101_bubbleSort.js
@@ -1,9 +1,11 @@
 const bubbleSort = (arr) => {
   let noSwaps = false;
   for (let i = arr.length; i > 0; i--) {
+    noSwaps = true;
     for (let j = 0; j < i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        noSwaps = false;
       }
       if (noSwaps === true) break;
     }
