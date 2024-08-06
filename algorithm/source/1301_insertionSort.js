faf0e4ad2a86761d003bd9135dbdadc007e40ca9
6lueparr0t <6lueparr0t@gmail.com>
2024-08-07 02:27:52 +0900

    feat: bubble, selection, insertion 추가

---
+++ algorithm/source/1301_insertionSort.js
@@ -0,0 +1,14 @@
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let currentVal = arr[i];
+
+    let j = i - 1;
+    for (; j >= 0 && arr[j] > currentVal; j--) {
+      arr[j + 1] = arr[j];
+    }
+    arr[j + 1] = currentVal;
+  }
+  return arr;
+}
+
+console.log(insertionSort([2, 1, 9, 76, 4])); // [1, 2, 4, 9, 76]
--- practice/etc_countCyclicPairs.js
+++ practice/etc_countCyclicPairs.js
@@ -1,3 +1,18 @@
+/**
+For a = [13, 5604, 31, 2, 13, 4560, 546, 654, 456], the output should be solution(a) = 5.
+
+There are 5 cyclic pairs of numbers - pairs which are equal to each other after cyclic shifts.
+
+a[0] = 13 and a[2] = 31 (i = 0 and j = 2),
+a[0] = 13 and a[4] = 13 (i = 0 and j = 4),
+a[2] = 31 and a[4] = 13 (i = 2 and j = 4),
+a[1] = 5604 and a[5] = 4560 (i = 1 and j = 5),
+a[6] = 546 and a[7] = 654 (i = 6 and j = 7)
+Note that a[6] = 546 and a[8] = 456 are not cyclic pairs — 546 can only be paired with cyclic shift of 546, 465 and 654.
+Also, note that a[5] = 4560 and a[8] = 456 are not cyclic pairs because they have different number of digits.
+
+*/
+
 function countCyclicPairs(arr) {
 
   let count = 0;
@@ -5,7 +20,7 @@ function countCyclicPairs(arr) {
     for (let j = i + 1; j < arr.length; j++) {
       let a = arr[i].toString();
       let b = arr[j].toString();
-      if (a.length === b.length && (a+a).includes(b)) {
+      if (a.length === b.length && (a + a).includes(b)) {
         count++;
       }
     }
