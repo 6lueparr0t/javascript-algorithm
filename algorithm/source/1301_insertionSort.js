bd8d500c099ce2f3b6d10460ac54377829278fd0
6lueparr0t <6lueparr0t@gmail.com>
2024-08-19 03:13:50 +0900

    feat: retry

--- algorithm/source/1301_insertionSort.js
+++ algorithm/source/1301_insertionSort.js
@@ -1,13 +1,15 @@
 function insertionSort(arr) {
-  for (let i = 1; i < arr.length; i++) {
+  for (let i = 0; i < arr.length; i++) {
     let currentVal = arr[i];
 
     let j = i - 1;
     for (; j >= 0 && arr[j] > currentVal; j--) {
       arr[j + 1] = arr[j];
     }
+
     arr[j + 1] = currentVal;
   }
+
   return arr;
 }
 
