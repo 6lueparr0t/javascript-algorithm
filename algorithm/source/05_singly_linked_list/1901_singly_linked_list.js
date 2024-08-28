8602f1ea3b6ac8f382d25fece4667191d2be97ee
6lueparr0t <6lueparr0t@gmail.com>
2024-08-29 02:22:01 +0900

    feat: update singly linked list

---
+++ algorithm/source/05_singly_linked_list/1901_singly_linked_list.js
@@ -0,0 +1,78 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+/*
+const first = new Node("Hi");
+first.next = new Node("there");
+first.next.next = new Node("how");
+first.next.next.next = new Node("are");
+first.next.next.next.next = new Node("you");
+first.next.next.next.next.next = new Node("doing?");
+
+console.log(first);
+console.log(first.next);
+console.log(first.next.next);
+console.log(first.next.next.next);
+console.log(first.next.next.next.next);
+console.log(first.next.next.next.next.next);
+*/
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  pop() {
+    if (!this.head) return undefined;
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
+  travaerse() {
+    let current = this.head;
+    while (current) {
+      console.log(current.data);
+      current = current.next;
+    }
+  }
+}
+
+const list = new SinglyLinkedList();
+list.push("Hello");
+list.push("Goodbye");
+list.push("!");
+
+console.log('before pop :');
+list.travaerse();
+list.pop();
+
+console.log('after pop :');
+list.travaerse();
