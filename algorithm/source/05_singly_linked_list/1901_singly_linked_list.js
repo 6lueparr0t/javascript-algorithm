998d5f8a097441c2b5fcd46a3b78431d69274f46
6lueparr0t <6lueparr0t@gmail.com>
2024-09-12 06:50:39 +0900

    feat: singly linked list - insert, remove, reverse 마무리

--- algorithm/source/05_singly_linked_list/1901_singly_linked_list.js
+++ algorithm/source/05_singly_linked_list/1901_singly_linked_list.js
@@ -119,12 +119,68 @@ class SinglyLinkedList {
   set(index, value) {
     if (index < 0 || index >= this.length) return null;
     let foundNode = this.get(index);
-    if(foundNode) {
-      foundNode.data = value ;
+    if (foundNode) {
+      foundNode.data = value;
       return true;
     }
 
     return false;
+  }
+
+  insert(index, value) {
+    if (index < 0 || index > this.length) return false;
+    if (index === this.length) return !!this.push(value);
+    if (index === 0) return !!this.unshift(value);
+
+    const newNode = new Node(value);
+    const prev = this.get(index - 1);
+    const temp = prev.next;
+    prev.next = newNode;
+    newNode.next = temp;
+
+    this.length++;
+    return true;
+  }
+
+  remove(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
+
+    const prev = this.get(index - 1);
+    const removed = prev.next;
+    prev.next = removed.next;
+
+    this.length--;
+    return removed;
+  }
+
+  reverse() {
+    let node = this.head;
+    this.head = this.tail;
+    this.tail = node;
+
+    let next = null;
+    let prev = null;
+
+    for (let i = 0; i < this.length; i++) {
+      next = node.next;
+      node.next = prev;
+      prev = node;
+      node = next;
+    }
+
+    return this;
+  }
+
+  print() {
+    let arr = [];
+    let current = this.head;
+    while (current) {
+      arr.push(current.data);
+      current = current.next;
+    }
+    console.log(arr);
   }
 }
 
